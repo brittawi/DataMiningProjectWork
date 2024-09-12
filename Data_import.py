44f00468082ec0f6b76ef860600087765c3c4aa1
Dropptimus <emillundin9@gmail.com>
2024-09-12 10:47:51 +0200

    Added slice plot

--- Data_import.py
+++ Data_import.py
@@ -1,22 +1,38 @@
 import nibabel as nib
 import matplotlib.pylab as plt
 import numpy as np
+import nilearn as nil
+import scipy.ndimage as ndi
+from nilearn import plotting
+
+def load_image(path):
+    img = nib.load(path)
+    data = img.get_fdata()
+    print(f"Image shape: {data.shape}")
+    print("Metadata:")
+    print(img.header)
+    return img, data
 
 def visualise_image(image):
     fig, axes = plt.subplots(7,7, figsize=(8,8))
     for i, ax in enumerate(axes.reshape(-1)):
-        ax.imshow(image[:,:,i])
+        ax.imshow(ndi.rotate(image[:,:,i], 90))
+    plt.axis("off")
+    plt.show()
+
+def plot_slice(image, slice):
+    plt.imshow(ndi.rotate(image[:,:,slice], 90))
+    #plt.axis('off')
     plt.show()
 
 #Visualise one training image 
 tr_path_img_001 = "/Users/emillundin/Desktop/D7043E/Project/Task08_HepaticVessel/imagesTr/hepaticvessel_001.nii.gz"
-tr_img_001 = nib.load(tr_path_img_001).get_fdata()
-print(tr_img_001.shape)
-visualise_image(tr_img_001)
+tr_img_001, tr_data_001 = load_image(tr_path_img_001)
+#visualise_image(tr_data_001)
+plot_slice(tr_data_001, 30)
 
 #Visualise one label image
 label_path_img_001 = "/Users/emillundin/Desktop/D7043E/Project/Task08_HepaticVessel/labelsTr/hepaticvessel_001.nii.gz"
-label_img_001 = nib.load(label_path_img_001).get_fdata()
-print(label_img_001.shape)
-visualise_image(label_img_001)
-    
+label_img_001, label_data_001 = load_image(label_path_img_001)
+#visualise_image(label_data_001)
+plot_slice(label_data_001, 30)
