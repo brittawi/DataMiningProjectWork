b398c22aeccef8c62723c5c2714190fcb6b827ca
Dropptimus <emillundin9@gmail.com>
2024-10-17 12:27:29 +0200

    Show stepwise transformation of one input image

--- Exploration_preprocessing/Data_import.py
+++ Exploration_preprocessing/Data_import.py
@@ -35,9 +35,9 @@ def plot_slice(image, slice):
 #Visualise one training image 
 tr_path_img_001 = "Data/Task01_BrainTumour/imagesTr/BRATS_001.nii.gz"
 tr_path_img_003 = "Data/Task02_Heart/imagesTr/la_003.nii.gz"
-tr_img_001, tr_data_001 = load_image(tr_path_img_003)
-visualise_image(tr_data_001)
-plot_slice(tr_data_001, 30)
+#tr_img_001, tr_data_001 = load_image(tr_path_img_003)
+#visualise_image(tr_data_001)
+#plot_slice(tr_data_001, 30)
 
 #Visualise one label image
 label_path_img_001 = "/Users/emillundin/Desktop/D7043E/Project/Task08_HepaticVessel/labelsTr/hepaticvessel_001.nii.gz"
